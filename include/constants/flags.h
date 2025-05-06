53aeeb0c80a85fbbcb3e9eafa2b81a942370b842
Rob T <Robert.Tootill98@gmail.com>
2025-05-06 20:59:14 +0100

    items on ferre grasslands

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1046,8 +1046,8 @@
 // Item Ball Flags
 // sundara village
 #define FLAG_ITEM_SUNDARA_POTION                                    0x3E8
-#define FLAG_ITEM_ROUTE_116_X_SPECIAL                               0x3E9
-#define FLAG_ITEM_ROUTE_104_PP_UP                                   0x3EA
+#define FLAG_ITEM_FERREGRASSLANDS_ANTIDOTE                          0x3E9
+#define FLAG_ITEM_FERREGRASSLANDS_POTION                            0x3EA
 #define FLAG_ITEM_ROUTE_105_IRON                                    0x3EB
 #define FLAG_ITEM_ROUTE_106_PROTEIN                                 0x3EC
 #define FLAG_ITEM_ROUTE_109_PP_UP                                   0x3ED
